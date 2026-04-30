a398fe9bee971f9fb0cb0b37af0a3b427198cbb4
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-04-30 18:48:11 +0200

    add proper error loging

--- backend/controllers/campusesController.js
+++ backend/controllers/campusesController.js
@@ -2,10 +2,10 @@ const pool = require("../config/db");
 
 exports.getCampuses = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM campuses ORDER BY id ASC");
+    const result = await pool.query("SELECT * FROM campuses");
     res.json(result.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
+    console.error("DB ERROR:", err);
+    res.status(500).json({ error: err.message });
   }
 };
