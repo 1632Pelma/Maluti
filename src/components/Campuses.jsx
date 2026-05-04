51a01cc637b4fb746a45da54eed0ecccda9fcd5d
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-05-04 15:15:42 +0200

    Update Campuses.jsx

--- src/components/Campuses.jsx
+++ src/components/Campuses.jsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Campuses.css";
 
 function Campuses() {
   const [campuses, setCampuses] = useState([]);
 
+  const API_URL = import.meta.env.VITE_API_URL;
+
   useEffect(() => {
-    fetch("https://maluti.onrender.com/api/campuses")
-      .then(res => res.json())
-      .then(data => setCampuses(data))
-      .catch(err => console.error(err));
+    fetch(`${API_URL}/api/campuses`)
+      .then((res) => res.json())
+      .then((data) => setCampuses(data))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
@@ -16,21 +18,21 @@ function Campuses() {
       <h2 className="campuses-title">Our Campuses</h2>
 
       <div className="campuses-container">
-        {campuses.map((campus) => (
-          <div className="campus-card" key={campus.id}>
-            <div className="campus-image-wrapper">
-              <img
-                src={campus.image}
-                alt={campus.name}
-                className="campus-image"
-              />
+        {Array.isArray(campuses) &&
+          campuses.map((campus) => (
+            <div className="campus-card" key={campus.id}>
+              <div className="campus-image-wrapper">
+                <img
+                  src={campus.image}
+                  alt={campus.name}
+                  className="campus-image"
+                />
+              </div>
+              <div className="campus-name">
+                <h3>{campus.name}</h3>
+              </div>
             </div>
-
-            <div className="campus-name">
-              <h3>{campus.name}</h3>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
