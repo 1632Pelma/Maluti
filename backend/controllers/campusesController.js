230b0ea8fee964604915717c4e12d3194336af6d
Pelma Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2026-05-04 15:14:56 +0200

    Update campusesController.js

--- backend/controllers/campusesController.js
+++ backend/controllers/campusesController.js
@@ -2,10 +2,10 @@ const pool = require("../config/db");
 
 exports.getCampuses = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM campuses");
+    const result = await pool.query("SELECT * FROM campuses ORDER BY id ASC");
     res.json(result.rows);
   } catch (err) {
-    console.error("DB ERROR:", err);
+    console.error("ERROR:", err);
     res.status(500).json({ error: err.message });
   }
 };
